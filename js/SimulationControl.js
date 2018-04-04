fc6b9bb06a3f73cbe7a58cb526a411e881fa1a02
Svana, Petr <petr.svana@siemens.com>
2018-04-04 20:43:36 +0200

    pause button functionality

--- js/SimulationControl.js
+++ js/SimulationControl.js
@@ -1,5 +1,9 @@
 $('#runSimulationButton').on("click", function () {
     runSimulation();
+    simulation.startVisualisation();
+});
+$('#pauseSimulationButton').on("click", function () {
+    simulation.pauseVisualisation();
 });
 $('#stopSimulationButton').on("click", function () {
     simulation = new Simulation();
